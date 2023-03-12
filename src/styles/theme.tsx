e04995abeac2105cc74785cd3267cac3affad5bf
Chae Sujong <phyzee@naver.com>
2023-03-12 15:50:02 +0900

    add emotion

---
+++ src/styles/theme.tsx
@@ -0,0 +1,11 @@
+import { Theme } from "@emotion/react";
+
+export const theme: Theme = {
+  color: {
+    tan: "#B68D40",
+    cream: "#F4EBD0",
+    charcoal: "#122620",
+    gold: "#D6AD60",
+  },
+  font: { main: "Pretendard" },
+};
